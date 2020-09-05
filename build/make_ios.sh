edf56af19199628fc55e7c4a3aeec82220941cc0
chenquanjun <chenshiyouxiang@163.com>
2020-09-05 16:06:02 +0800

    Update make_ios.sh

--- build/make_ios.sh
+++ build/make_ios.sh
@@ -1,6 +1,33 @@
-mkdir -p build_ios && cd build_ios
-cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/iOS.cmake  -GXcode ../
-cd ..
+#!/bin/bash
 
-cmake --build build_ios --config Release
-cp build_ios/Release-iphoneos/libslua.a ../Assets/Plugins/iOS/libslua.a
+CMAKE=cmake
+
+function make_for() {
+	ROOT=$1
+	BUILD_DIR=$ROOT/$2
+	TOOLCHAIN=$ROOT/cmake/ios/ios.toolchain.cmake
+
+	if [ ! -d $BUILD_DIR ]; then
+		mkdir $BUILD_DIR
+	fi
+
+	cd $BUILD_DIR
+
+	$CMAKE -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN   \
+		   -DIOS_PLATFORM="OS"                 \
+		   -DIOS_DEPLOYMENT_TARGET="8.0"       \
+		   -DENABLE_BITCODE="true"             \
+		   -G Xcode                            \
+		   $ROOT
+
+	cd $ROOT
+
+	$CMAKE --build $BUILD_DIR --config Release
+}
+
+make_for `pwd` build_ios
+
+if [ "$?" == "0" ]; then
+	cp -v build_ios/Release-iphoneos/libslua.a ../Assets/Plugins/iOS/libslua.a
+fi
+
